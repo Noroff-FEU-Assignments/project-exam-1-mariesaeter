cca13432f44126a0ea7593772584cd0a993a37ae
mariesaeter <marie.saeter@gmail.com>
2022-11-13 20:44:31 +0100

    hamburger

--- js/sitewide/hamburger.js
+++ js/sitewide/hamburger.js
@@ -1,42 +1,67 @@
-const navText = document.querySelector(".nav-text");
-const menu = document.querySelector(".menu");
+const nav = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+// const menu = document.querySelector(".menu");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerIcon = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close");
 const menuItems = document.querySelectorAll(".menu-item");
 
-function toggleMenu() {
-  if (menu.classList.contains("openMenu")) {
-    menu.classList.remove("openMenu");
-    menu.style.display = "none";
-    closeIcon.style.display = "none";
-    hamburgerIcon.style.display = "block";
-  } else {
-    menu.classList.add("openMenu");
-    menu.style.display = "block";
-    closeIcon.style.display = "block";
-    hamburgerIcon.style.display = "none";
-    menu.style.backgroundImage = "url('../../images/nav-background.jpg')";
-  }
+hamburgerMenu.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburgerMenu.classList.toggle("active");
+  nav.classList.toggle("active");
 }
 
-hamburgerMenu.addEventListener("click", toggleMenu);
+navLink.forEach((link) => link.addEventListener("click", closeMenu));
 
-// Change event to make the hamburger manu function only on smaller sizes
-// inspiration: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/matches
-let mql = window.matchMedia("(max-width:767px)");
-console.log(mql);
+function closeMenu() {
+  hamburgerMenu.classList.remove("active");
+  nav.classList.remove("active");
+}
+// function myFunction() {
+//   let x = document.getElementById("nav");
+//   if (x.className === "nav") {
+//     x.className += " menu";
+//   } else {
+//     x.className = "nav";
+//   }
+// }
 
-mql.addEventListener("change", (event) => {
-  if (event.matches) {
-    navText.style.display = "none";
+// hamburgerIcon.addEventListener("click", myFunction);
 
-    hamburgerMenu.addEventListener("click", toggleMenu);
-  } else {
-    navText.style.display = "flex";
-    menu.style.display = "none";
-  }
-});
+// // function toggleMenu() {
+// //   if (menu.classList.contains("openMenu")) {
+// //     menu.classList.remove("openMenu");
+// //     menu.style.display = "none";
+// //     closeIcon.style.display = "none";
+// //     hamburgerIcon.style.display = "block";
+// //   } else {
+// //     menu.classList.add("openMenu");
+// //     menu.style.display = "block";
+// //     closeIcon.style.display = "block";
+// //     hamburgerIcon.style.display = "none";
+// //     menu.style.backgroundImage = "url('../../images/nav-background.jpg')";
+// //   }
+// // }
+
+// // hamburgerMenu.addEventListener("click", toggleMenu);
+
+// // // Change event to make the hamburger manu function only on smaller sizes
+// // // inspiration: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/matches
+// // let mql = window.matchMedia("(max-width:767px)");
+// // console.log(mql);
+
+// // mql.addEventListener("change", (event) => {
+// //   if (event.matches) {
+// //     navText.style.display = "none";
+
+// //     hamburgerMenu.addEventListener("click", toggleMenu);
+// //   } else {
+// //     navText.style.display = "flex";
+// //     menu.style.display = "none";
+// //   }
+// // });
 
 // menuItems.forEach(function (menuItem) {
 //   menuItem.addEventListener("click", toggleMenu);
