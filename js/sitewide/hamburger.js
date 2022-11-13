d548c353ea04b464d90b4265f6f36c2bdc8e163f
mariesaeter <marie.saeter@gmail.com>
2022-11-13 16:13:33 +0100

    finished js for navigation

--- js/sitewide/hamburger.js
+++ js/sitewide/hamburger.js
@@ -1,32 +1,9 @@
+const navText = document.querySelector(".nav-text");
 const menu = document.querySelector(".menu");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerIcon = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close");
 const menuItems = document.querySelectorAll(".menu-item");
-
-let mql = window.matchMedia("(min-width:767px)");
-
-console.log(mql);
-
-mql.addEventListener("change", (event) => {
-  if (event.matches) {
-    toggleMenu();
-  } else {
-    menu.style.display = "block";
-    menu.style.backgroundImage = "none";
-  }
-});
-
-// mql.addEventListener(handleMediaChange, "change");
-// handleMediaChange(mql);
-// let handleMediaChange = function (mediaQueryList) {
-//   if (mediaQueryList.matches) {
-//     toggleMenu();
-//   } else {
-//     menu.classList.remove("openMenu");
-//     menu.style.display = "flex";
-//   }
-// };
 
 function toggleMenu() {
   if (menu.classList.contains("openMenu")) {
@@ -42,8 +19,66 @@ function toggleMenu() {
     menu.style.backgroundImage = "url('../../images/nav-background.jpg')";
   }
 }
+
 hamburgerMenu.addEventListener("click", toggleMenu);
 
-menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener("click", toggleMenu);
+// Change event to make the hamburger manu function only on smaller sizes
+// inspiration: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/matches
+let mql = window.matchMedia("(max-width:767px)");
+console.log(mql);
+
+mql.addEventListener("change", (event) => {
+  if (event.matches) {
+    navText.style.display = "none";
+
+    hamburgerMenu.addEventListener("click", toggleMenu);
+  } else {
+    navText.style.display = "flex";
+    menu.style.display = "none";
+  }
 });
+
+// menuItems.forEach(function (menuItem) {
+//   menuItem.addEventListener("click", toggleMenu);
+// });
+
+// // tried to do it with only 1 navigation but failed
+
+// const nav = document.querySelector(".navigation");
+
+// function toggleMenu() {
+//   if (nav.classList.contains("openMenu")) {
+//     nav.classList.remove("openMenu");
+//     nav.style.display = "none";
+//     closeIcon.style.display = "none";
+//     hamburgerIcon.style.display = "block";
+//   } else {
+//     nav.classList.add("openMenu");
+//     nav.classList.add("menu");
+//     nav.classList.remove("nav-text");
+//     nav.style.display = "block";
+//     closeIcon.style.display = "block";
+//     hamburgerIcon.style.display = "none";
+//     nav.style.backgroundImage = "url('../../images/nav-background.jpg')";
+//   }
+// }
+
+// hamburgerMenu.addEventListener("click", toggleMenu);
+
+// // Change event to make the hamburger manu function only on smaller sizes
+// // inspiration: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/matches
+// let mql = window.matchMedia("(max-width:767px)");
+// console.log(mql);
+// mql.addEventListener("change", (event) => {
+//   if (event.matches) {
+//     nav.classList.add("menu");
+//     nav.classList.remove("nav-text");
+//     navText.style.display = "none";
+
+//     hamburgerMenu.addEventListener("click", toggleMenu);
+//   } else {
+//     navText.style.display = "flex";
+//     menu.style.display = "none";
+//     nav.classList.remove("menu");
+//   }
+// });
